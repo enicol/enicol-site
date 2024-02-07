300c896121b9186f88c5d1ea63fd55c797d55505
Emma Nicol <emma@yolk.ai>
2024-02-07 16:32:33 -0500

    Update color config and add social svgs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,23 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "black-green": "#3B413C",
+        "olive-green": "#b7b898",
+        tarragon: {
+          100: "#CCCCB7",
+          200: "#C4C4AB",
+          300: "#b7b898",
+          400: "#B3B493",
+          500: "#A2A37B",
+        },
+        "soft-white": "#ede8e5",
+        "rich-blue": "#36558F",
+        barley: "#C2C6FF",
+        chamomile: "#e0c3a1",
+        saffron: "#cb864f",
+        grain: "#8a7971",
+      },
     },
   },
   plugins: [],
