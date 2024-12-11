c1bf230464bbea53b4cf69e4175e1eb418159cb4
Emma Nicol <emmaenicol@gmail.com>
2024-12-11 12:35:46 -0500

    Move icons back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
     <>
       <main className="flex min-h-screen flex-col items-center h-screen p-14 bg-gradient-to-l from-[#716f81] via-[#b97a95] to-[#f6ae99]">
         <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-start justify-center dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none ">
+          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none ">
             <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 lg:mr-4 text-rich-blue hover:text-saffron"
+              className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 lg:mr-4 text-rich-blue hover:text-saffron"
               href="https://github.com/enicol"
               target="_blank"
               rel="noopener noreferrer"
@@ -23,7 +23,7 @@ export default function Home() {
               />
             </a>
             <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 hover:text-sky-700"
+              className="flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 hover:text-sky-700"
               href="https://linkedin.com/in/enicol"
               target="_blank"
               rel="noopener noreferrer"
